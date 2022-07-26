1fcbed366364da0e412fad023e0d37f25a76ec02
nazmieievilya <ilyanazmeev111@gmail.com>
2022-07-26 15:35:02 +0300

    Added icons and animations

--- js/script.js
+++ js/script.js
@@ -6,27 +6,47 @@ const getExchangeButton = document.getElementById('getExchange');
 const exchangeRateResult = document.querySelector('.exchange-rate');
 const fromElement = document.getElementById('from-Droplist');
 const toElement = document.getElementById('to-Droplist');
-
+const fromImg = document.getElementById('fromImage');
+const toImg = document.getElementById('toImage');
+const switcher = document.getElementById('switch');
+const switchImge = document.querySelector('.fas');
 let fromInput = 'USD';
 let toInput = 'UAH';
+
 fromElement.value = fromInput;
 toElement.value = toInput;
 let amountInput = 1;
 
+fromImg.src = `pics/${fromInput}.png`;
+toImg.src = `pics/${toInput}.png`;
+
+function currencySwitcher() {
+  let switchVar = fromInput;
+  // replacing api request values
+  fromInput = toInput;
+  toInput = switchVar;
+  switchImge.classList.add('rotate-center');
+  // updating select values
+  fromElement.value = fromInput;
+  toElement.value = toInput;
+  fromImg.src = `pics/${fromInput}.png`;
+  toImg.src = `pics/${toInput}.png`;
+
+  getExchangeRate();
+  window.setTimeout(function () {
+    switchImge.classList.remove('rotate-center');
+  }, 300);
+}
+
 function equalutyListener() {
   // Create variables you will reassign if select on change and comparing if they are equals
   if (fromElement.value === toElement.value) {
-    let switchVar = fromInput;
-    // replacing api request values
-    fromInput = toInput;
-    toInput = switchVar;
-
-    // updating select values
-    fromElement.value = fromInput;
-    toElement.value = toInput;
+    currencySwitcher();
   } else {
     fromInput = fromElement.value;
     toInput = toElement.value;
+    fromImg.src = `pics/${fromInput}.png`;
+    toImg.src = `pics/${toInput}.png`;
   }
 }
 
@@ -38,7 +58,6 @@ dropListTo.addEventListener('change', e => {
 });
 
 function getExchangeRate() {
-  console.log('Clicked');
   amountInput = document.getElementById('amount').value;
   var requestURL = `https://api.exchangerate.host/convert?from=${fromInput}&to=${toInput}&amount=${amountInput}`;
   var request = new XMLHttpRequest();
@@ -54,3 +73,4 @@ function getExchangeRate() {
 
 getExchangeRate();
 getExchangeButton.addEventListener('click', getExchangeRate);
+switcher.addEventListener('click', currencySwitcher);
